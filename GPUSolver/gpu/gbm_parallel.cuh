3bbbaf27980f945a2b5a35b87a008147276f515f
chemardes <cdmrs08@gmail.com>
2025-04-26 20:43:00 +0100

    FEATURE: fix time monitoring for gbm

--- GPUSolver/gpu/gbm_parallel.cuh
+++ GPUSolver/gpu/gbm_parallel.cuh
@@ -130,11 +130,12 @@ public:
 template<typename T = DEFAULT_FPX>
 SimulationResults<T> simulate(T initial_stock_price, T mu, T sigma, T time, int num_of_simulations, int time_steps)
 {
+    auto start = std::chrono::high_resolution_clock::now();
+
     size_t grid_size = num_of_simulations * time_steps;
 
     T *dev_grid;
     gpuErrChk(cudaMalloc(&dev_grid, grid_size * sizeof(T)));
-    auto start = std::chrono::high_resolution_clock::now();
 
     simulate_gbm<<<numBlocks(num_of_simulations), THREADS_PER_BLOCK>>>(dev_grid, initial_stock_price, mu, sigma, time, time_steps, num_of_simulations);
     gpuErrChk(cudaDeviceSynchronize());
